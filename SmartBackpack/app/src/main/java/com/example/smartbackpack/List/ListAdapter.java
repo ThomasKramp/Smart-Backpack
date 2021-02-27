76de9976b5a632d4701414939c045a1f17da7ff8
Thomas Kramp <S115472@ap.be>
2021-02-27 13:54:29 +0100

    Create Recyclerreview Layout and Classes

---
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListAdapter.java
@@ -0,0 +1,24 @@
+package com.example.smartbackpack.List;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ListAdapter extends RecyclerView.Adapter<ListViewHolder> {
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.smartbackpack.List;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.smartbackpack.R;
+
+import java.util.LinkedList;
+
+public class ListViewHolder extends RecyclerView.ViewHolder {
+
+    public ListViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}

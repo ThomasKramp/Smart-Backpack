668775e1cc975c3ac37da996a79288f23b300533
Thomas Kramp <S115472@ap.be>
2021-02-27 14:34:17 +0100

    Added basic listAdapter & listViewHolder functions

--- SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListAdapter.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListAdapter.java
@@ -1,24 +1,42 @@
 package com.example.smartbackpack.List;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.smartbackpack.ListActivity;
+import com.example.smartbackpack.R;
+
 public class ListAdapter extends RecyclerView.Adapter<ListViewHolder> {
+
+    String[] vItemNames;
+    int[] vItemAmounts;
+
+    LayoutInflater mInflater;
+
+    public ListAdapter(ListActivity listActivity, String[] itemNames, int[] itemAmounts) {
+        mInflater = LayoutInflater.from(listActivity);
+        vItemNames = itemNames;
+        vItemAmounts = itemAmounts;
+    }
+
     @NonNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View viewHolder = mInflater.inflate(R.layout.list_item_layout, parent, false);
+        return new ListViewHolder(viewHolder);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
-
+        holder.mNameView.setText(vItemNames[position]);
+        holder.mAmountView.setText(vItemAmounts[position]);
+        holder.mImageView.setImageResource(R.drawable.ic_launcher_foreground);
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public int getItemCount() { return vItemAmounts.length; }
 }
--- SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListViewHolder.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/List/ListViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.smartbackpack.List;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,11 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartbackpack.R;
 
-import java.util.LinkedList;
-
 public class ListViewHolder extends RecyclerView.ViewHolder {
+
+    TextView mNameView;
+    TextView mAmountView;
+    ImageView mImageView;
 
     public ListViewHolder(@NonNull View itemView) {
         super(itemView);
+        mNameView = itemView.findViewById(R.id.list_item_name);
+        mAmountView = itemView.findViewById(R.id.list_item_amount);
+        mImageView = itemView.findViewById(R.id.list_item_image);
     }
 }

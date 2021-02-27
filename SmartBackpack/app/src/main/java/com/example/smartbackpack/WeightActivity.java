f7e38815882f6487b1fe3d1677196c608fc3b7b1
Thomas <s112981@ap.be>
2021-02-27 11:02:51 +0100

    Added a basic function in Weight activity

--- SmartBackpack/app/src/main/java/com/example/smartbackpack/WeightActivity.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/WeightActivity.java
@@ -3,12 +3,35 @@ package com.example.smartbackpack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class WeightActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class WeightActivity extends AppCompatActivity
+{
+    private TextView ShowMeasurement;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight);
+
+        ShowMeasurement = (TextView) findViewById(R.id.MeasuredData);
+    }
+
+    public void measureData(View view)
+    {
+        ShowMeasurement.setText(RandomWeight(view));
+    }
+
+    public String RandomWeight(View view)
+    {
+        int min = 2;
+        int max = 35;
+        int RandomWeight = (int)(Math.random() * (max - min + 1) + min);
+
+        return String.valueOf(RandomWeight) + "kg";
     }
 }

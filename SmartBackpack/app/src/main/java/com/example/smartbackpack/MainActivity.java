621e8f87c9b642512d5505612c91f4e9109726ae
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2021-02-27 14:21:21 +0100

    Add log messages

--- SmartBackpack/app/src/main/java/com/example/smartbackpack/MainActivity.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startMoistureActivity(View view) {
+        Log.d(TAG, "Go to the moisture activity");
         /*
         Intent messageIntent = new Intent(this, MoistureActivity.class);
         startActivity(messageIntent);
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startWeightActivity(View view) {
+        Log.d(TAG, "Go to the weight activity");
         /*
         Intent messageIntent = new Intent(this, WeightActivity.class);
         startActivity(messageIntent);
@@ -29,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startListActivity(View view) {
+        Log.d(TAG, "Go to the list activity");
         /*
         Intent messageIntent = new Intent(this, ListActivity.class);
         startActivity(messageIntent);

6e7048a684800504fb068e95bf4632fe3d74b5b2
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2021-02-27 14:08:19 +0100

    Added intents to the buttons

--- SmartBackpack/app/src/main/java/com/example/smartbackpack/MainActivity.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.smartbackpack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startMoistureActivity(View view) {
+        /*
+        Intent messageIntent = new Intent(this, MoistureActivity.class);
+        startActivity(messageIntent);
+         */
+    }
+
+    public void startWeightActivity(View view) {
+        /*
+        Intent messageIntent = new Intent(this, WeightActivity.class);
+        startActivity(messageIntent);
+         */
+    }
+
+    public void startListActivity(View view) {
+        /*
+        Intent messageIntent = new Intent(this, ListActivity.class);
+        startActivity(messageIntent);
+         */
     }
 }

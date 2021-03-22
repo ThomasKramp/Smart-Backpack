be2fe7c91c8bfd231def72af5ec0c75f9cee5320
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2021-03-22 23:00:15 +0100

    Enable bluetooth switch

--- SmartBackpack/app/src/main/java/com/example/smartbackpack/Bluetooth/BluetoothFragment.java
+++ SmartBackpack/app/src/main/java/com/example/smartbackpack/Bluetooth/BluetoothFragment.java
@@ -1,24 +1,30 @@
 package com.example.smartbackpack.Bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.smartbackpack.R;
 
 public class BluetoothFragment extends Fragment {
-    public BluetoothFragment() {
-        // Required empty public constructor
-    }
+    private BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+    private Switch btSwitch;
+    private static String BT_ON_MESSAGE = "Bluetooth enabled";
+    private static String BT_OFF_MESSAGE = "Bluetooth disabled";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -26,5 +32,48 @@ public class BluetoothFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bluetooth, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btSwitch = view.findViewById(R.id.bt_switch);
+
+        if (btAdapter != null) {
+            if (btAdapter.isEnabled()) {
+                btSwitch.setChecked(true);
+                showToast(BT_ON_MESSAGE);
+            } else {
+                btSwitch.setChecked(false);
+            }
+        } else {
+            showToast("Device does not support bluetooth");
+        }
+
+        // If user toggles the bt switch
+        btSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    enableBluetooth();
+                    showToast(BT_ON_MESSAGE);
+                } else {
+                    disableBluetooth();
+                    showToast(BT_OFF_MESSAGE);
+                }
+            }
+        });
+    }
+
+    private void disableBluetooth() {
+        btAdapter.disable();
+    }
+
+    private void enableBluetooth() {
+        btAdapter.enable();
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 }
